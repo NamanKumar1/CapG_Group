3c47896b5e14351d3afd5f9490a424be096ca23d
NamanKumar1 <naman.kumar_cs19@gla.ac.in>
2023-05-31 20:04:52 +0530

    add commment

--- UserInterface.java
+++ UserInterface.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/**import java.util.Scanner;
 
 public class UserInterface {
     public static void main(String[] args) {
@@ -55,3 +55,4 @@ public class UserInterface {
 
     }
 }
+**/
